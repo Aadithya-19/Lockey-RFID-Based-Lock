81a050b8795ba730a19e562efb7e7337c013f722
Aadithya Vasudevan <svaadithya@gmail.com>
2026-04-22 14:33:17 -0400

    new library

--- src/pins.h
+++ src/pins.h
@@ -2,30 +2,32 @@
 #define PINS_H
 
 
-#define SPI_PORT    spi1
-#define PIN_MISO    12
-#define PIN_MOSI    11
-#define PIN_SCK     10
-#define PIN_CS      13
-#define PIN_RST     9
+#define SPI_PORT spi1
+#define PIN_MISO 12
+#define PIN_MOSI 11
+#define PIN_SCK 10
+#define PIN_CS 13
+#define PIN_RST 9
 
 
 #define PIN_LED_GREEN 19
 #define PIN_LED_RED 22
-#define PIN_BUZZER 23
+#define PIN_BUZZER 26
 
 
-#define PIN_SWITCH      23
-#define PIN_SERVO       24
+#define PIN_SWITCH 23
+#define PIN_SERVO 24
 
-#define LCD_SPI_PORT    spi0
-#define PIN_LCD_SCK     34
-#define PIN_LCD_TX      35
-#define PIN_LCD_CSn     33
-#define PIN_LCD_DC      6
-#define PIN_LCD_RESET   7
 
-#define STORE_SDA       4
-#define STORE_SCL       5
+//we used a tft
+#define LCD_SPI_PORT spi0
+#define PIN_LCD_SCK 34
+#define PIN_LCD_TX 35
+#define PIN_LCD_CSn 33
+#define PIN_LCD_DC 25
+#define PIN_LCD_RESET 27
+
+#define STORE_SDA 4
+#define STORE_SCL 5
 
 #endif
